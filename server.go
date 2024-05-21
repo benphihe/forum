cfa8bcb4c027f34ba0723247463afbaf3ad412c2
dcamille <camilledouaud@ynov.Com>
2024-05-21 10:05:00 +0200

    serveur start

--- server.go
+++ server.go
@@ -1,5 +1,35 @@
 package forum
 
-func server() {
+import (
+	"fmt"
+	"html/template"
+	"net/http"
+)
 
+func Server() {
+	http.HandleFunc("/register", Register)
+	http.HandleFunc("/login", Login)
+	http.HandleFunc("/login-form", LoginForm)
+	http.HandleFunc("/register-form", RegisterForm)
+	http.HandleFunc("/", Home)
+	fileServer := http.FileServer(http.Dir("templates/assets/"))
+	http.Handle("/assets/", http.StripPrefix("/assets", fileServer))
+	fmt.Println("http://localhost:8000/  Server is running in port 8000")
+	http.ListenAndServe(":8000", nil)
+}
+
+func Home(w http.ResponseWriter, r *http.Request) {
+	tmpl, error := template.ParseGlob("templates/*.html")
+	if error != nil {
+		panic(error)
+	}
+	tmpl.ExecuteTemplate(w, "index", nil)
+}
+
+func Forum(w http.ResponseWriter, r *http.Request) {
+	tmpl, error := template.ParseGlob("templates/*.html")
+	if error != nil {
+		panic(error)
+	}
+	tmpl.ExecuteTemplate(w, "main", nil)
 }
