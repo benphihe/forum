d97a32d94097ed71078e7f07910da9b12991daa5
camcam <150005586+camcarre@users.noreply.github.com>
2024-05-31 07:41:26 +0200

    debug

--- SRC/Forum/bddduser.go
+++ SRC/Forum/bddduser.go
@@ -15,7 +15,7 @@ func Open() {
 	}
 
 	var err error
-	db, err = sql.Open("sqlite3", "BDD/users.db")
+	db, err = sql.Open("sqlite3", "./BDD/users.db")
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -24,6 +24,9 @@ func Open() {
 func CreateUser(pseudo string, password string, email string) error {
 	log.Printf("CreateUser a reçu : pseudo=%s, password=%s, email=%s\n", pseudo, password, email)
 	_, err := db.Exec("insert into Utilisateurs (pseudo, password, email) values (?, ?, ?)", pseudo, password, email)
+	if err != nil {
+		log.Printf("Erreur lors de l'insertion des données : %s\n", err)
+	}
 	return err
 }
 
