c01c3d018be723db842d366bd1523bcef6c9d92a
camcam <150005586+camcarre@users.noreply.github.com>
2024-06-10 10:46:08 +0200

    chore: Remove unused code and files

--- server.go
+++ server.go
@@ -18,12 +18,6 @@ func main() {
 	http.HandleFunc("/connexion", Forum.Connexion)
 	http.HandleFunc("/cookies", Forum.SignOutHandler)
 
-	http.Handle("/user", Forum.AuthMiddleware(http.HandlerFunc(Forum.UserHandler)))
-	http.Handle("/comment", Forum.AuthMiddleware(http.HandlerFunc(Forum.AddComment)))
-	http.Handle("/post", Forum.AuthMiddleware(http.HandlerFunc(Forum.AddPost)))
-	http.Handle("/tweet", Forum.AuthMiddleware(http.HandlerFunc(Forum.AddTweet)))
-	http.Handle("/comment_tweet", Forum.AuthMiddleware(http.HandlerFunc(Forum.CommentTweet)))
-
 	http.ListenAndServe(":8080", nil)
 	fmt.Println("Server Start in localhost:8080")
 }
