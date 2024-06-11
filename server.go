ed60e2863bd1f12923596bdeb7107ae28d4d08eb
camcam <150005586+camcarre@users.noreply.github.com>
2024-06-11 15:04:43 +0200

    merge theo cam

--- server.go
+++ server.go
@@ -10,10 +10,14 @@ import (
 func main() {
 	fs := http.FileServer(http.Dir("STATIC"))
 	http.Handle("/STATIC/", http.StripPrefix("/STATIC/", fs))
+	fs := http.FileServer(http.Dir("STATIC"))
+	http.Handle("/STATIC/", http.StripPrefix("/STATIC/", fs))
 
 	Forum.Open()
 	http.HandleFunc("/", AuthMiddleware(Forum.DisplayPostsFrombdd))
 	http.HandleFunc("/user", AuthMiddleware(Forum.DisplayUserInfo))
+	http.HandleFunc("/user", AuthMiddleware(Forum.UserHandler))
+	http.HandleFunc("/post/", AuthMiddleware(Forum.DisplayPost))
 	http.HandleFunc("/inscription", Forum.InscriptionPage)
 	http.HandleFunc("/connexion", Forum.Connexion)
 	http.HandleFunc("/addpost", AuthMiddleware(Forum.AddPost))
@@ -22,9 +26,18 @@ func main() {
 
 	http.ListenAndServe(":8080", nil)
 	fmt.Println("Server Start in localhost:8080")
+	http.ListenAndServe(":8080", nil)
+	fmt.Println("Server Start in localhost:8080")
 }
 
 func PostHandler(w http.ResponseWriter, r *http.Request) {
+	if r.Method == http.MethodGet {
+		Forum.DisplayPost(w, r)
+	} else if r.Method == http.MethodPost {
+		Forum.AddComment(w, r)
+	} else {
+		http.Error(w, "Invalid request method", http.StatusMethodNotAllowed)
+	}
 	if r.Method == http.MethodGet {
 		Forum.DisplayPost(w, r)
 	} else if r.Method == http.MethodPost {
